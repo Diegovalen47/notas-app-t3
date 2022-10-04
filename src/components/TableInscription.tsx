195d14e241148f6ce752d470e67dbd0d4b13a10b
Diegovalen47 <osoriovalentin5@gmail.com>
2022-10-04 15:18:45 -0500

    Estilos de tabla

---
+++ src/components/TableInscription.tsx
@@ -0,0 +1,35 @@
+import { NextComponentType } from "next";
+import { trpc } from "../utils/trpc";
+import styles from "../styles/TableInscription.module.css";
+
+const TableInscription: NextComponentType = () => {
+  const { data: list, refetch } = trpc.useQuery(["inscription.findAll"]);
+  if (!list) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <table className={`${styles.contenedor}`}>
+      <tr className={`${styles.fila} ${styles.tableBorders}`}>
+        <th className={styles.columna}>Asignatura</th>
+        <th className={styles.columna}>Creditos</th>
+        <th className={styles.columna}>Tipologia</th>
+        <th className={styles.columna}>Semestre</th>
+      </tr>
+      {list.map((inscription) => {
+        return (
+          <tr key={inscription.inscriptionId} className={styles.fila}>
+            <td className={styles.columna}>{inscription.course.name}</td>
+            <td className={styles.columna}>{inscription.course.credits}</td>
+            <td className={styles.columna}>{inscription.typology.name}</td>
+            <td className={styles.columna}>
+              {`${inscription.semester.year} 
+                ${inscription.semester.period}
+                ${inscription.semester.number}`}
+            </td>
+          </tr>
+        );
+      })}
+    </table>
+  );
+}
+export default TableInscription;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import TableInscription from "../components/TableInscription";
 
 const Home: NextPage = () => {
-  const { data: list, refetch } = trpc.useQuery(["inscription.findAll"]);
-  if (!list) {
-    return <div>Loading...</div>;
-  }
   return (
-    <>
-      {list.map((inscription) => {
-        return (
-          <div className="columns" key={inscription.inscriptionId}>
-            <div className="column">{inscription.course.name}</div>
-            <div className="column">{inscription.course.credits}</div>
-            <div className="column">{inscription.typology.name}</div>
-            <div className="column">
-              {`${inscription.semester.year} 
-                ${inscription.semester.period}
-                ${inscription.semester.number}`}
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <TableInscription />
   );
 };
-
 export default Home;
